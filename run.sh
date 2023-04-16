4a68f919cf8d2b974a82d0487b91c3ad8727e2ce
Anya-gh <Aniket.Singh@warwick.ac.uk>
2023-04-16 15:07:36 +0100

    Fixed batch files

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python -m venv env
 source env/bin/activate
-pip install torch tensorboard pandas pretty_midi pyyaml tqdm protobuf==3.19.6
+pip3 install torch tensorboard pandas pretty_midi pyyaml tqdm protobuf==3.19.6
 tensorboard --logdir=log & python train_gan.py asap.yaml
